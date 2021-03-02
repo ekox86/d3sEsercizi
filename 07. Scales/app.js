90dd4ff55bb065a9c2ae4c0bb624b8490e0c1166
Antonio Girasella <agirasella@reer.it>
2021-03-02 08:32:37 +0100

    Commenti su scale

--- 07. Scales/app.js
+++ 07. Scales/app.js
@@ -1,9 +1,11 @@
+//Definisco i dati in input
 const slices = [100, 200, 300, 400, 500]
 
+//Scegliamo una scala lineare in questo caso, ma d3 ha anche altre funzioni di scalatura.
 const scale = d3.scaleLinear()
-  //.domain([d3.min(slices), d3.max(slices)])
-  .domain(d3.extent(slices))
-  .range([10, 350])
+  //.domain([d3.min(slices), d3.max(slices)]) //domain definisce min e max di input della scala.
+  .domain(d3.extent(slices))                  //extent prende automaticamente il minimo e il massimo dell'array
+  .range([10, 350])                           //range è l'output della funzione di scala. 
 
   console.log(scale(100)) //print 10
   console.log(scale(500)) //print 350
