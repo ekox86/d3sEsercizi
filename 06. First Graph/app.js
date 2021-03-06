83c670115e9ae88f055bbd747418768e10397cf3
Antonio Girasella <a.girasella@gmail.com>
2021-03-06 12:37:13 +0100

    Evoluzioni scale

--- 06. First Graph/app.js
+++ 06. First Graph/app.js
@@ -3,8 +3,8 @@ async function draw() {
     // Data 
     const dataset = await d3.json('data.json');
 
-    const xAccessor = (d)=> d.currently.humidity
-    const yAccessor = (d)=> d.currently.apparentTemperature
+    const xAccessor = (d) => d.currently.humidity
+    const yAccessor = (d) => d.currently.apparentTemperature
 
     //Dimensions
     let dimensions = {
@@ -16,24 +16,34 @@ async function draw() {
             left: 50,
             right: 50
         }
-    }  
-
+    }
+    dimensions.ctrWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
+    dimensions.ctrHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
     //Draw Image
     const svg = d3.select('#chart')
         .append('svg')
-        .attr('width',dimensions.width)
-        .attr('height',dimensions.height)
+        .attr('width', dimensions.width)
+        .attr('height', dimensions.height)
     const ctr = svg.append('g').
-        attr('transform',`translate(${dimensions.margin.left},${dimensions.margin.top})` )
-
-    //Draw Circle
+        attr('transform', `translate(${dimensions.margin.left},${dimensions.margin.top})`)
+    //Scales
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, xAccessor))
+        .rangeRound([0, dimensions.ctrWidth]) //rangeRound arrotonda l'output
+        .clamp(true) //clamp disabilita la visualizzazione di valori che sono fuori dal range visualizzabile
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, yAccessor))
+        .rangeRound([0, dimensions.ctrHeight]) //rangeRound arrotonda l'output
+        .nice() //arrotonda l'input all'intero più vicino
+        .clamp(true)
+        //Draw Circles
     ctr.selectAll('circle')
         .data(dataset)
         .join('circle')
-        .attr('cx', xAccessor)
-        .attr('cy', yAccessor)
-        .attr('r',5)
-        .attr('fill','red')
+        .attr('cx', d => xScale(xAccessor(d)))
+        .attr('cy', d => yScale(yAccessor(d)))
+        .attr('r', 5)
+        .attr('fill', 'red')
 }
 
-draw ()
+draw()
