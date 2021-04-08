4c53c5743c798502fe64278a47b3b95aef833bd9
Antonio Girasella <agirasella@reer.it>
2021-04-08 13:28:49 +0200

    Line chart: aggiunta generazione linea, aggiunti assi orizzontale e verticale.

--- 23. Line Chart/app.js
+++ 23. Line Chart/app.js
@@ -43,5 +43,31 @@ async function draw() {
   const xScale = d3.scaleUtc() //a differenza di scaleTime, ScaleUtc lavora con date utc
     .domain(d3.extent(dataset, xAccessor)) //la extent funziona anche con le date: ritorna la data più vecchia e la più nuova.
     .range([0,dimensions.ctrWidth])    
+
+//LINE GENERATION
+  const lineGenerator = d3.line()     //crea una funzione che genera una linea, un path svg
+    .x((d) => xScale(xAccessor(d)))   //in questo modo indico dove leggere le x dei punti della linea dall'array.
+    .y((d) => yScale(yAccessor(d)))   //in questo modo indico dove leggere le y dei punti della linea dall'array.
+    //console.log(lineGenerator(dataset)) //stampa tutto il path della linea
+    ctr.append('path')  //il valore generato dal linegenerator è compativile solo con un svg path
+      .datum(dataset)   //invece di usare data, usiamo datum perchè associamo un singolo dato al path.
+      .attr('d',lineGenerator)  //la funzione lineGenerator verrà applicata al dataset creando la linea. L'attributo d del path svg descrive i punti del path.
+      .attr('fill', 'none')     //in questo modo il contenuto del path non viene riempito, di default i path svg sono riempiti di nero.
+      .attr('stroke','#30475e') //d'altronde se non diamo un colore allo stroke non vedremo la linea di contorno del path, quindi dobbiamo settarlo.
+      .attr('stroke-width',2)   //cambiamo lo spessore della linea
+
+//AXIS 
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(8)
+      .tickFormat(d3.timeFormat("%m/%d/%y"))  //formattiamo la data sui tick sull'asse orizzontale
+    const xAxisGroup = ctr.append('g')
+    .call(xAxis) 
+    .style('transform',`translateY(${dimensions.ctrHeight}px)`)
+
+    const yAxis = d3.axisLeft(yScale)
+      .tickFormat((d) =>`$${d}`)  //aggiungiamo il simbolo dollaro al tick sull'asse verticale
+    const yAxisGroup = ctr.append('g')
+      .call(yAxis)
+
   }
 draw()
