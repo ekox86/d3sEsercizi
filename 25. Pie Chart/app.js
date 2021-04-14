81e95589b2338f205f54f812a7ecb79c924199a8
Antonio Girasella <agirasella@reer.it>
2021-04-14 13:22:05 +0200

    Terminate labels su piechart e riordinate slices torta

--- 25. Pie Chart/app.js
+++ 25. Pie Chart/app.js
@@ -27,8 +27,8 @@ async function draw() {
   // Scales
   //In questo caso le scale ci servono per capire quanto è grossa una fetta della torta.
   const populationPie = d3.pie() //ritorna una funzione che organizza i dati in fette.
-      .value((d) => d.value);//questa funzione dirà a d3.pie come accedere ai dati del dataset. in questo modo potrà capire come calcolare la grandezza delle fette.
-  
+      .value((d) => d.value)//questa funzione dirà a d3.pie come accedere ai dati del dataset. in questo modo potrà capire come calcolare la grandezza delle fette.
+      .sort(null);            //di default i dati vengono ordinati in base al valore passato. Possiamo scegliere di non riordinarli o di ordinarli in base a un altro criterio. 
   const slices = populationPie(dataset);  //le slices sono le fette della torta, indicate con angolo di start e angolo di end di ogni fetta.
   //ogni fetta ha un index che indica l'ordine di disegno,
                                           //i valori degli angoli sono misurati in radianti. 
@@ -47,6 +47,9 @@ async function draw() {
        //L'arco sarà disegnato a partire dal centro del container, col raggio che gli abbiamo passato.
        .innerRadius(0)           //serve a fare un grafico a ciambella. 
   
+  const arcLabels = d3.arc()  //questa funzione arco serve per posizionare le labels
+      .outerRadius(radius)    // ai bordi del grafico a torta. Useremo infatti poi la funzione arc.centroid
+      .innerRadius(200)       // per mettere le labels a metà della distanza dal centro.
   // Draw Shape
   const arcGroup = ctr.append('g') //disegnamo in un gruppo che posizioniamo al centro del container.
       .attr('transform', `translate(${dimensions.ctrHeight/2},${dimensions.ctrWidth/2})`)
@@ -64,12 +67,17 @@ async function draw() {
       labelsGroup.selectAll('text')
         .data(slices)
         .join('text')
-        .attr('transform',(d) => `translate(${arc.centroid(d)})`) //il centroid ritorna le coordinate x, y del centro dell'arco, cioè a metà srada tra il centro e la circoferenza.
+        .attr('transform',(d) => `translate(${arcLabels.centroid(d)})`) //il centroid ritorna le coordinate x, y del centro dell'arco, cioè a metà srada tra il centro e la circoferenza.
         .call( text => text.append('tspan') 
                         .style('font-weight','bold') 
-                        .text(d=>d.data.name)
+                        .attr('y',-4)
+                        .text(d=>d.data.name) //spostiamo in alto il nome del dato
             )                 //la call permette di eseguire una funzione sulla selezione originale senza cambiare la selezione che viene ritornata.
-        .call( text => text.append('tspan') 
+        .call( text => text.filter((d) => (d.endAngle - d.startAngle) > 0.25)      //filter ritorna una nuova d3selection filtrata secondo la funzione passata com parametro
+                                                                                  //In questo caso, il valore viene mostrato solo negli spicchi più larghi di un certo angolo. 
+                        .append('tspan') 
+                        .attr('y',9)        //spostiamo in basso il valore del dato. 
+                        .attr('x',0)        // resettiamo a 0 la coordinata per allinearla con quella dello span col nome
                         .text(d=>d.data.value)
         )
         
